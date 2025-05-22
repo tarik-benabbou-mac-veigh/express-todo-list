3cdc9ab971bc2084f9dacae570aaf8c0f84b607d
Tarik <t2.macben@gmail.com>
2025-05-23 00:41:35 +0800

    Method post has been added to add a new task

--- views/app.js
+++ views/app.js
@@ -18,14 +18,37 @@ const tasks = [
         title: 'Learn MongoDB',
         done: false,
     },
+    {
+        title: 'Learn Express',
+        done: false,
+    },
 ];
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
+// POST method :
+app.post('/task', (req, res)=>{
+    tasks.push(
+        {
+            title: req.body.task,
+            done: false,
+        },
+    );
+    res.redirect('/');
+});
+
+// GET methods :
 app.get('/', (req,res)=>{
     res.render('todolist', {tasks});
+});
+
+app.get('/task/:id/done', (req,res)=>{
+    tasks[req.params.id].done = true;
+    res.redirect('/');
 });
 
 app.listen(port, ()=>{
