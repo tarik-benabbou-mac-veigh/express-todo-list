b4a4f1d73a103cbb04db8f1eb0ba7fa7dd51818b
Tarik <t2.macben@gmail.com>
2025-05-24 00:08:08 +0800

    Added a condition to the POST '/task' route to prevent adding empty tasks. Also created two new GET routes: one to toggle the status of a task and another to delete a task from the todo list.

--- views/app.js
+++ views/app.js
@@ -3,15 +3,23 @@ const app = express();
 const port = 4000;
 const tasks = [
     {
+        title: 'Learn HTML',
+        done: true,
+    },
+    {
+        title: 'Learn CSS',
+        done: true,
+    },
+    {
+        title: 'Learn JavaScript',
+        done: true,
+    },
+    {
         title: 'Learn Nodejs',
         done: true,
     },
     {
-        title: 'Learn SQL',
-        done: true,
-    },
-    {
-        title: 'Learn PHP',
+        title: 'Learn Express',
         done: false,
     },
     {
@@ -19,7 +27,11 @@ const tasks = [
         done: false,
     },
     {
-        title: 'Learn Express',
+        title: 'Learn SQL',
+        done: false,
+    },
+    {
+        title: 'Learn PHP',
         done: false,
     },
 ];
@@ -31,13 +43,17 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // POST method :
+/*Adding a task*/
 app.post('/task', (req, res)=>{
-    tasks.push(
-        {
-            title: req.body.task,
-            done: false,
-        },
-    );
+    /*condition to avoid having an empty task in the list*/
+    if(req.body.task){
+        tasks.push(
+            {
+                title: req.body.task,
+                done: false,
+            },
+        );
+    };
     res.redirect('/');
 });
 
@@ -46,10 +62,17 @@ app.get('/', (req,res)=>{
     res.render('todolist', {tasks});
 });
 
+/*Change the status of a task*/
 app.get('/task/:id/done', (req,res)=>{
     tasks[req.params.id].done = true;
     res.redirect('/');
 });
+
+/*Delete a task from the list*/
+app.get('/task/:id/delete', (req,res)=>{
+    tasks.splice(req.params.id, 1);
+    res.redirect('/')
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port${port}`);
